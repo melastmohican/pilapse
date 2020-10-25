9b6d63667cf858716e800e313d9741f510a5a0fd
Mariusz S. Jurgielewicz <mordor@mail.com>
2020-10-25 15:18:13 -0700

    Playing with mock camera

--- mock_picamera/camera.py
+++ mock_picamera/camera.py
@@ -7,11 +7,18 @@ class PiCamera(object):
 
     def __init__(self, *args, **kwargs):
         print('WARNING: Mocked PiCamera on {}'.format(os.uname()))
+        self.resolution = None
+        self.shutter_speed = None
+        self.exposure_speed = None
+        self.awb_gains = None
         pass
 
     def start_preview(self, **options):
-        self.resolution = None
         pass
+
+    def stop_preview(self):
+        pass
+
 
     def capture_continuous(self, output, format=None, use_video_port=False, resize=None,
                            splitter_port=0, burst=False, bayer=False, **options):
--- timelapse.py
+++ timelapse.py
@@ -1,17 +1,18 @@
 import os
 from datetime import datetime
 from time import sleep
-
-#if os.uname()[1] == 'raspberrypi':
 from picamera import PiCamera
-#else:
-#    from mock_picamera.camera import PiCamera
+# if os.uname()[1] == 'raspberrypi':
+#     from picamera import PiCamera
+# else:
+#     from mock_picamera.camera import PiCamera
 
 
 def get_path(base_dir):
     full_path = base_dir + '/' + datetime.now().strftime('%Y/%m/%d/%H')
     os.makedirs(full_path, exist_ok=True)
     return full_path
+
 
 # print(os.uname()[1])
 base_dir = '/var/image'
@@ -27,7 +28,9 @@ g = camera.awb_gains
 camera.awb_mode = 'off'
 camera.awb_gains = g
 camera.start_preview()
-sleep(2)
-for filename in camera.capture_continuous(get_path(base_dir) + '/img{counter:05d}.jpg'):
-    print('Captured %s' % filename)
-    sleep(60)  # wait 1 minutes
+try:
+    for filename in camera.capture_continuous(get_path(base_dir) + '/img{counter:05d}.jpg'):
+        print('Captured %s' % filename)
+        sleep(60)  # wait 1 minutes
+finally:
+    camera.stop_preview()
